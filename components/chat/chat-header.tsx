c8761d46ed2318f7ddea51f610882ddd3adbe2c0
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-09-14 19:08:07 +0300

    chat header

---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,23 @@
+import MobileToggle from "@/components/mobile-toggle";
+import { Hash } from "lucide-react";
+
+interface Props {
+  serverId: string;
+  name: string;
+  type: "channel" | "conversation";
+  imageUrl?: string;
+}
+
+const ChatHeader = ({ serverId, name, type, imageUrl }: Props) => {
+  return (
+    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <MobileToggle serverId={serverId} />
+      {type === "channel" && (
+        <Hash className="w-5 h5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+    </div>
+  );
+};
+
+export default ChatHeader;
